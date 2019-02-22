1ba71156a98a8fba90d741abe1cecc47f1987531
Jordan Rogers <jordan.rogers@iop.org>
2019-02-22 14:15:22 +0000

    Change getalbum to getAlbum()

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,7 +13,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor( private _productService: ProductService ) { }
 
   ngOnInit() {
-    this._productService.getalbum(1)
+    this._productService.getAlbum(1)
       .subscribe( response => this.albumInfo = response )
   }
 
