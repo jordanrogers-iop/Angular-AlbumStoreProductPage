765c094751592b8e52eea36cd4d0a3a772c39ce8
Jordan Rogers <jordan.rogers@iop.org>
2019-02-25 14:37:57 +0000

    Switch to HttpClient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
@@ -29,6 +30,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { HttpClient } from "@angular/common/http";
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
@@ -11,14 +12,14 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   private _productsUrl = '../assets/products.json';
 
-  getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map( response => <Album>response.json() );
+  getAlbum(id: number): Observable<Album[]> {
+    return this._httpClient.get<Album[]>(this._albumUrl);
   }
 
-  getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map( response => <Product[]>response.json() );
+  getProducts(): Observable<Product[]> { //functions return an Observable of type Product[]. This allows to use the map operator on the get request below.
+    return this._httpClient.get<Product[]>(this._productsUrl); //specify the expected TYPE of response to an array of Product. The response object is then automatically mapped to the defined type.
   }
 
-  constructor( private _http: Http ) { }
+  constructor( private _http: Http, private _httpClient: HttpClient ) { }
 
 }
